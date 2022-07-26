efbe76aacee8e53dc7952f6f8b0ef7ee3b701515
Matthew OBrien <mobrien@broadinstitute.org>
2022-07-26 15:36:23 -0400

    fix import error

--- bibt/gcp/scc/__init__.py
+++ bibt/gcp/scc/__init__.py
@@ -2,6 +2,7 @@ from bibt.gcp.scc.scc import get_all_assets
 from bibt.gcp.scc.scc import get_all_findings
 from bibt.gcp.scc.scc import get_asset
 from bibt.gcp.scc.scc import get_finding
+from bibt.gcp.scc.scc import get_value
 from bibt.gcp.scc.scc import parse_notification
 from bibt.gcp.scc.scc import set_finding_state
 from bibt.gcp.scc.scc import set_security_mark
@@ -12,6 +13,7 @@ __all__ = (
     "get_all_assets",
     "get_all_findings",
     "get_asset",
+    "get_value",
     "get_finding",
     "parse_notification",
     "set_finding_state",
